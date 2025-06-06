2c1f8b4cf99ed67e0e26cfa48058f604448681aa
Roman <roman.mainn@gmail.com>
2025-06-07 02:54:06 +0500

    onchain tests are not all completed yet

--- scripts/sendMessage.ts
+++ scripts/sendMessage.ts
@@ -5,7 +5,9 @@ import { NetworkProvider, sleep } from '@ton/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('kQBmfyvtydi0NBEofnMQHnbPcQW23jsD4SS2EnpoPU4C9EBh'));
+    const address = Address.parse(
+        args.length > 0 ? args[0] : await ui.input('kQBmfyvtydi0NBEofnMQHnbPcQW23jsD4SS2EnpoPU4C9EBh'),
+    );
 
     if (!(await provider.isContractDeployed(address))) {
         ui.write(`Error: Contract at address ${address} is not deployed!`);
@@ -17,11 +19,9 @@ export async function run(provider: NetworkProvider, args: string[]) {
     const MessageBefore = await myContract.getContractData();
     let random = Math.floor(Math.random() * 100);
 
-    await myContract.sendMessageEdit(
-        provider.sender(),
-        toNano("0.01"),
-        `TEST MESSAGE ONCHAIN ${random}`.toString(),
-    );
+    //await myContract.sendMessageEdit(provider.sender(), toNano('0.01'), `TEST MESSAGE ONCHAIN №${random}`.toString());
+    //await myContract.sendDeleteMessage(provider.sender(), toNano('0.02'));
+    await myContract.sendChangeAccess(provider.sender(), toNano('0.02'), 0);
 
     ui.write('Waiting for message to edit...');
 
@@ -36,5 +36,7 @@ export async function run(provider: NetworkProvider, args: string[]) {
 
     ui.clearActionPrompt();
     ui.write('Counter increased successfully!');
-    console.log(`FULL DATA:\nOwner: ${MessageAfter.owner}\nAccess: ${MessageAfter.access}\nRecent sender: ${MessageAfter.recent_sender}\nMessage: ${MessageAfter.message}\nMessage time: ${MessageAfter.message_time}`)
+    console.log(
+        `FULL DATA:\nOwner: ${MessageAfter.owner}\nAccess: ${MessageAfter.access}\nRecent sender: ${MessageAfter.recent_sender}\nMessage: ${MessageAfter.message}\nMessage time: ${MessageAfter.message_time}`,
+    );
 }
